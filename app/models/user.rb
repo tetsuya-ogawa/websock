24085be0f6f1b326f9df72c4ef2b8deeda5bfe01
徹也小川 <tetsuyaogawa@tetsuyaogawa-no-MacBook-Pro.local>
2017-06-03 20:59:23 +0900

    ルームの作成機能その他主なビュー

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ApplicationRecord
-
   has_secure_password
-  
+
+  has_many :user_rooms
+  has_many :rooms, through: :user_rooms
+
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
 end
