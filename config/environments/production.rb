eddbdb29f554094e93ee75cd3af8117677efd378
徹也小川 <tetsuyaogawa@tetsuyaogawa-no-MacBook-Pro.local>
2017-06-04 19:29:04 +0900

    デプロイ修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.web_socket_server_url = "wss://t-t-message.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = ['https://t-t-message.herokuapp.com/', 'http://t-t-message.herokuapp.com/']
+
 end
